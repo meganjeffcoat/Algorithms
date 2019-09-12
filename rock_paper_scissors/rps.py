d96a1c3207f979de58872cd1e6d6dc6b20be5265
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-09-12 17:55:04 -0500

    rps complete

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -20,7 +20,24 @@ import sys
 
 
 def rock_paper_scissors(n):
-    pass
+    if n == 0:
+        return [[]]
+
+    result = []
+    num_of_plays = [0]*n  # n is the number of plays
+    trigger_rps(num_of_plays, 0, result)
+    return result
+
+
+def trigger_rps(arr, index, result):
+
+    if index == len(arr):
+        result.append(list(arr))
+        return arr
+
+    for item in ("rock", "paper", "scissors"):
+        arr[index] = item
+        trigger_rps(arr, index+1, result)
 
 
 if __name__ == "__main__":
